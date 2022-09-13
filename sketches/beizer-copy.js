26d0c6c901a2d5bd9c910cb8f274d7317f833d47
gscottqueen <geoffreysqueen@gmail.com>
2022-09-13 10:42:55 -0400

    v1

--- sketches/beizer-copy.js
+++ sketches/beizer-copy.js
@@ -7,36 +7,36 @@ const removeZeroData = (array) => array.filter(item => item !== 0 )
 
 const filterdData = removeZeroData(data["data"])
 
-const sampleData = filterdData.slice(0, filterdData.length / 100)
+const sampleData = filterdData.slice(0, filterdData.length / 300)
 
 const settings = {
   dimensions: 'A4',
-  orientation: 'portrait',
-  animate: true,
-  duration: 50
+  orientation: 'portrait'
 }
 
 canvasSketch((props) => {
-  const { context, width, height, units } = props
+  const { width, height, units } = props
   // We will add to this over time
-  let t = Math.sin(Math.PI * 2);
-  console.log(sampleData)
   const paths = [];
 
-  sampleData.forEach(i => {
+  let t = Math.sin(Math.PI);
+
+  sampleData.forEach((i) => {
     const x = t
     const y = t
     const w = width / 2
     const h = height / 2
+    const frequency = 2
+    const amplitude = 1
 
-    const x1 = w * Random.noise2D(x, y - 15);
-    const x2 = w * Random.noise2D(x, y - 25);
-    const x3 = w * Random.noise2D(x, y - 35);
-    const x4 = w * Random.noise2D(x, y - 45);
-    const y1 = h * Random.noise2D(x, y - 55);
-    const y2 = h * Random.noise2D(x, y - 65);
-    const y3 = h * Random.noise2D(x, y - 75);
-    const y4 = h * Random.noise2D(x, y - 85);
+    const x1 = w * Random.noise2D(x, y - 15, frequency, amplitude);
+    const x2 = w * Random.noise2D(x, y - 25, frequency, amplitude);
+    const x3 = w * Random.noise2D(x, y - 35, frequency, amplitude);
+    const x4 = w * Random.noise2D(x, y - 45, frequency, amplitude);
+    const y1 = h * Random.noise2D(x, y - 55, frequency, amplitude);
+    const y2 = h * Random.noise2D(x, y - 65, frequency, amplitude);
+    const y3 = h * Random.noise2D(x, y - 75, frequency, amplitude);
+    const y4 = h * Random.noise2D(x, y - 85, frequency, amplitude);
 
     const p = createPath();
     const centerWidth = width / 2
@@ -53,7 +53,7 @@ canvasSketch((props) => {
       y4 + centerHeight);
     paths.push(p);
 
-    t += 0.005;
+    t += 0.002;
   })
 
   let lines = pathsToPolylines(paths, { units });
@@ -63,7 +63,7 @@ canvasSketch((props) => {
       lineJoin: 'round',
       lineCap: 'round',
       // in working units; you might have a thicker pen
-      lineWidth: 0.08,
+      lineWidth: 1,
       // Optimize SVG paths for pen plotter use
       optimize: true
     });
